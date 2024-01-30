6849f9607f4dc6886de291a43a7d21ce1f499b73
Brian Ragazzi <brianpragazzi@gmail.com>
2024-01-30 11:09:45 -0600

    Create buildsshimage.sh

---
+++ buildsshimage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+docker build -t openrewritedemossh . -f dockerfilessh
+
+#docker tag concourse-kubectl:180121 harbor.ragazzilab.com/library/concourse-kubectl:180121
+#docker push harbor.ragazzilab.com/library/concourse-kubectl:180121
+
+docker run -it --rm \
+  --name openrewrite-demo-ssh \
+  -v "$(pwd):/data" \
+  -v openrewrite-root-m2:/root/.m2 \
+  -v openrewrite-root-sdkman-candidates:/root/.sdkman/candidates \
+  -w /data \
+  -p 22:8022 \
+  openrewritedemossh /bin/bash
+  #./demo.sh
